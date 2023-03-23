d92dae65da205128863cd067ac90591f8bb12b36
Talibiq123 <talibsaeed90@gmail.com>
2023-03-23 17:05:53 +0530

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("This is from Linked List Lectures.");
+        System.out.println("This is from Linked List Lectures...");
     }
 }
