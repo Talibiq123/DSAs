6a36172dec4646437f6450a6ad57b0c7d434040e
Talibiq123 <talibsaeed90@gmail.com>
2023-04-03 17:14:10 +0530

    commit

--- src/stackandqueue/CustomStack.java
+++ src/stackandqueue/CustomStack.java
@@ -1,4 +1,51 @@
 package stackandqueue;
 
 public class CustomStack {
+    protected int[] data;
+    private static final int DEFAULT_SIZE = 10;
+
+    int ptr;
+
+    public CustomStack() {
+        this(DEFAULT_SIZE);
+    }
+
+    public CustomStack(int size) {
+        this.data = new int[size];
+    }
+    
+    public boolean push(int item) {
+        if (isFull()) {
+            System.out.println("Stack is Full!");
+            return false;
+        }
+        ptr++;
+        data[ptr] = item;
+        return true;
+    }
+
+    public int pop() throws StackException {
+        if (isEmpty()) {
+            throw new StackException("can not pop from empty stack");
+        }
+//        int removed = data[ptr];
+//        ptr--;
+//        return removed;
+        return data[ptr--];
+    }
+
+    public int peek() throws StackException {
+        if (isEmpty()) {
+            throw new StackException("can not peep from empty stack");
+        }
+        return data[ptr];
+    }
+
+    public boolean isFull() {
+        return ptr == data.length - 1;
+    }
+
+    private boolean isEmpty() {
+        return ptr == - 1;
+    }
 }
