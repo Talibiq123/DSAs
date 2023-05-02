67276291128e12d7f46e1f7177e59f66d0ebfa33
Talibiq123 <talibsaeed90@gmail.com>
2023-05-02 13:07:11 +0530

    commit

---
+++ src/arrays/ArrayCC.java
@@ -0,0 +1,28 @@
+package arrays;
+
+//import java.util.*;
+
+import java.util.Scanner;
+
+public class ArrayCC {
+    public static void main(String[] args) {
+
+        //int[] input = new int[50];
+        //int[] input = {1, 2, 3, 4, 5};
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("Enter the Elements of array");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Elements of Array ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] +" ");
+        }
+    }
+}
