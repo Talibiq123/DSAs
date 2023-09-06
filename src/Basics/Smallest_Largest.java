666d46e844e43a16c570f685a68da3f7d9eb1d33
Talibiq123 <talibsaeed90@gmail.com>
2023-09-06 22:08:22 +0530

    string

---
+++ src/Basics/Smallest_Largest.java
@@ -0,0 +1,17 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class Smallest_Largest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter three numbers ");
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        int num3 = sc.nextInt();
+//        int largest = largest(num1, num2, num3);
+//        int smallest = smallest(num1, num2, num3);
+//        System.out.println("Largest "+largest);
+//        System.out.println("Smallest "+smallest);
+//    }
+}
---
+++ src/strings/DuplicateCharacters.java
@@ -0,0 +1,8 @@
+// 1. Print duplicate characters from String?
+package strings;
+
+public class DuplicateCharacters {
+    public static void main(String[] args) {
+
+    }
+}
